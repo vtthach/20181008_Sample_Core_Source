819fae7ef6a60d63b53b208794c30ab4ccc7e556
longle <long.le@hubcba.com>
2018-10-16 14:31:40 +0700

    RAIN-20-dashboard

---
+++ app/src/main/java/com/innovation/rain/feature/rica/base/BaseRicaFragment.kt
@@ -0,0 +1,33 @@
+package com.innovation.rain.feature.rica.base
+
+import android.os.Bundle
+import android.view.View
+
+import com.innovation.rain.app.base.fragment.BasePresenterInjectionFragment
+import com.innovation.rain.app.base.presenter.BasePresenter
+import com.innovation.rain.app.enums.RicaState
+import com.innovation.rain.feature.agentlogin.view.RicaHomeFragment
+import com.innovation.rain.feature.rica.home.callback.RicaStateView
+
+abstract class BaseRicaFragment<T : BasePresenter> : BasePresenterInjectionFragment<T>(), RicaStateView {
+
+    var ricaState: RicaState? = null
+        set(ricaState) {
+            field = ricaState
+            when (ricaState) {
+                RicaState.STATE_PRE_LOADED -> onRicaStatePreLoad()
+                RicaState.STATE_LOADED -> onRicaStateLoaded()
+                RicaState.STATE_DONE -> onRicaStateDone()
+            }
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ricaState = RicaState.STATE_PRE_LOADED
+    }
+
+    protected fun enableButtonProceed(allowEnableProceedButton: Boolean) {
+        val f = parentFragment as RicaHomeFragment?
+        f?.enableButtonProceed(allowEnableProceedButton)
+    }
+}

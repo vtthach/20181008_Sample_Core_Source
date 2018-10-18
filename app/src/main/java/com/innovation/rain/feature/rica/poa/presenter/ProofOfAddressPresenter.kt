d508a78d21703615f19956274463da9c07a07b6a
ThachVo <thach.vo@hubcba.com>
2018-10-18 13:29:10 +0700

    RAIN-4 - Fix refactor package namem

--- app/src/main/java/com/innovation/rain/feature/rica/poa/presenter/ProofOfAddressPresenter.kt
+++ app/src/main/java/com/innovation/rain/feature/rica/poa/presenter/ProofOfAddressPresenter.kt
@@ -1,6 +1,6 @@
 package com.innovation.rain.feature.rica.poa.presenter
 
-import com.innovation.rain.app.base.presenter.BasePresenter
+import com.sf0404.core.application.base.presenter.BasePresenter
 
 interface ProofOfAddressPresenter : BasePresenter {
     fun handleProceedButtonClicked()

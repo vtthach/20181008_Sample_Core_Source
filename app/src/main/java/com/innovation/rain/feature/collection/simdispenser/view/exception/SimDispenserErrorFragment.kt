8f3f1d87887a785fc3fab99c48cdda1de809bd21
longle <long.le@hubcba.com>
2018-10-24 16:25:58 +0700

    RAIN-10

--- app/src/main/java/com/innovation/rain/feature/collection/simdispenser/view/exception/SimDispenserErrorFragment.kt
+++ app/src/main/java/com/innovation/rain/feature/collection/simdispenser/view/exception/SimDispenserErrorFragment.kt
@@ -3,7 +3,7 @@ package com.innovation.rain.feature.collection.signin.view.exception
 import android.os.Bundle
 import android.view.View
 import com.innovation.rain.R
-import com.innovation.rain.app.util.NavigateUtil
+import com.innovation.rain.app.utils.NavigateUtil
 import com.sf0404.common.dialog.fragment.BaseDialogFragment
 import com.sf0404.common.fragment.util.BundleUtils
 import kotlinx.android.synthetic.main.collection_client_login_no_order_fragment.*

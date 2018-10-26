6cce18ab5fe149b96fb6684849a2743f9b6f12be
AnhVu <anh.vu@hubcba.com>
2018-10-26 11:08:11 +0700

    fix spelling

--- app/src/main/java/com/innovation/rain/app/injection/module/model/AppBus.kt
+++ app/src/main/java/com/innovation/rain/app/injection/module/model/AppBus.kt
@@ -2,10 +2,10 @@ package com.innovation.rain.app.injection.module.model
 
 import com.innovation.rain.feature.collection.orders.model.OrderEntity
 
-data class AppBus(var orderList: List<OrderEntity>? = null, var flow: Flow = Flow.UN_DEFINED)
+data class AppBus(var orderList: List<OrderEntity>? = null, var flow: Flow = Flow.UNDEFINED)
 
 enum class Flow {
     SHOP,
     COLLECT,
-    UN_DEFINED
+    UNDEFINED
 }
